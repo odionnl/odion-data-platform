0801aaa3211b4db167c6573867b667b582f10728
Chantal van Son <c.vanson@odion.nl>
2025-12-17 16:57:02 +0100

    simplify stuff by creating fct_client_woonsituatie

--- models/intermediate/clienten/int_clients_addresses.sql
+++ models/intermediate/clienten/int_clients_addresses.sql
@@ -15,33 +15,18 @@ lst_address_types as (
 final as (
     select
         ca.client_id,
+        a.adres_id,
         a.straatnaam,
         a.huisnummer,
         a.woonplaats,
         a.gemeente,
-        lst_at.adrestype,
-
-        -- bij verschillende adressen: kies meest relevante
-        ROW_NUMBER() OVER (
-            PARTITION BY ca.client_id
-            ORDER BY
-                CASE lst_at.adrestype
-                    WHEN 'GBA adres (woonadres)' THEN 1
-                    WHEN 'Verblijfadres' THEN 2
-                    WHEN 'Tijdelijk verblijfadres' THEN 3
-                    ELSE 99
-                END,
-                a.startdatum_adres DESC,
-                a.adres_id DESC
-        ) AS rn
+        a.startdatum_adres,
+        a.einddatum_adres,
+        lst_at.adrestype
     from clients_addresses ca
     join addresses a on a.adres_id = ca.adres_id
     join lst_address_types lst_at on lst_at.adrestype_code = a.adrestype_code
-    where lst_at.adrestype in ('GBA adres (woonadres)', 'Verblijfadres', 'Tijdelijk verblijfadres')
-      and a.startdatum_adres < getdate()
-      and (a.einddatum_adres is null or a.einddatum_adres > getdate())
-      and a.straatnaam is not null
+
 )
 
-select * from final
-where rn = 1;
+select * from final;
