b058330a0f410095383a4eef56b18fb9c16a8a97
Chantal van Son <c.vanson@odion.nl>
2026-01-28 10:57:12 +0100

    add jeugdtrajecten zonder verwijzer

---
+++ analyses/jeugdtrajecten_verwijzers.sql
@@ -0,0 +1,19 @@
+USE Ons_Plan_2;
+
+DECLARE @startdatum DATE = '2025-07-01';
+DECLARE @einddatum  DATE = '2025-12-31';
+
+SELECT
+    j.clientObjectId,
+    c.identificationNo AS clientnummer,
+    h.description as hulpvorm,
+    v.description as verwijzer,
+    j.datumAanvang,
+    j.datumBeeindiging
+FROM cbsbj_jeugdhulps j
+    LEFT JOIN cbsbj_hulpvormen h ON h.code = j.hulpvorm
+    LEFT JOIN cbsbj_verwijzers v ON v.code = j.verwijzer
+    LEFT JOIN clients c ON c.objectId=j.clientObjectId
+WHERE j.datumAanvang <= @einddatum
+    AND (j.datumBeeindiging >= @startdatum OR j.datumBeeindiging IS NULL)
+    AND v.description = 'Geen verwijzer'
