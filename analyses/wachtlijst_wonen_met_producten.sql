813a70f506854aa9ff4245d3f1149c13269c15e3
Chantal van Son <c.vanson@odion.nl>
2026-04-01 09:59:09 +0200

    use mart_wachtlijsten in analyse wachtlijst_wonen_met_producten

--- analyses/wachtlijst_wonen_met_producten.sql
+++ analyses/wachtlijst_wonen_met_producten.sql
@@ -1,112 +1,36 @@
 -- Analyse: cliënten op de wachtlijst Wonen en hun huidige producten (locatieclusters)
--- Direct uitvoerbaar in SSMS (geen dbt compile nodig)
--- TODO: onderscheid maken tussen wachtlijst wonen + wachtlijst logeren
-
--- Materialiseer actieve_koppelingen als temp table (wordt 3x gebruikt)
-drop table if exists #actieve_koppelingen;
+-- Gebruikt mart_wachtlijsten + mart_clienten
 
 select
-    lk.client_id,
-    lk.locatie_id,
-    lk.startdatum,
-    lk.einddatum,
-    lk.type_toekenning,
-    l.locatienaam,
-    l.cluster,
-    l.niveau3
+    wl.client_id,
+    wl.clientnummer,
 
-into #actieve_koppelingen
-
-from [OdionDataPlatform].[dbo].[stg_onsdb__location_assignments] lk
-    inner join [OdionDataPlatform].[dbo].[int_locatie_hierarchie] l
-    on l.locatie_id = lk.locatie_id;
-
-create index ix_ak_client on #actieve_koppelingen (client_id);
-
-with
-    wachtlijst_clienten
-    as
-    (
-
-        select
-            client_id,
-            locatie_id    as wachtlijst_locatie_id,
-            locatienaam   as wachtlijst_locatienaam,
-            startdatum    as wachtlijst_startdatum,
-            einddatum     as wachtlijst_einddatum
-
-        from #actieve_koppelingen
-        where cluster = N'Wachtlijsten'
-            and niveau3 in (N'LG (Wachtlijst)', N'VG (Wachtlijst)')
-            and (einddatum is null or einddatum >= cast(getdate() as date))
-
-    ),
-
-    hoofdlocaties
-    as
-    (
-
-        select
-            client_id,
-            locatie_id    as hoofdlocatie_locatie_id,
-            locatienaam   as hoofdlocatie_locatienaam,
-            row_number() over (
-            partition by client_id
-            order by startdatum desc
-        ) as rn
-
-        from #actieve_koppelingen
-        where type_toekenning = N'MAIN'
-            and (einddatum is null or einddatum >= cast(getdate() as date))
-
-    )
-
-select
-    c.client_id,
-    c.clientnummer,
-
-    c.in_zorg,
     c.leeftijd,
+    c.leeftijdsgroep,
+    c.in_zorg,
 
-    hl.hoofdlocatie_locatie_id,
-    hl.hoofdlocatie_locatienaam,
+    wl.hoofdlocatie_locatie_id,
+    wl.hoofdlocatie_locatienaam,
 
     wl.wachtlijst_locatie_id,
     wl.wachtlijst_locatienaam,
+    wl.wachtlijst_niveau3,
     wl.wachtlijst_startdatum,
     wl.wachtlijst_einddatum,
 
-    -- Huidige producten (1/0 per locatiecluster)
-    max(case when ak.cluster = N'Ambulant'               then 1 else 0 end) as [Ambulant],
-    max(case when ak.cluster = N'Dagbesteding'            then 1 else 0 end) as [Dagbesteding],
-    max(case when ak.cluster = N'Logeren'                 then 1 else 0 end) as [Logeren],
-    max(case when ak.cluster = N'Wonen'                   then 1 else 0 end) as [Wonen],
-    max(case when ak.cluster = N'Behandeling'             then 1 else 0 end) as [Behandeling],
-    max(case when ak.cluster = N'Kind en gezin'           then 1 else 0 end) as [Kind en gezin],
-    max(case when ak.cluster = N'Multidisciplinair Team'  then 1 else 0 end) as [Multidisciplinair Team]
+    wl.ambulant,
+    wl.dagbesteding,
+    wl.logeren,
+    wl.wonen,
+    wl.behandeling,
+    wl.kind_en_gezin,
+    wl.multidisciplinair_team
 
-from wachtlijst_clienten wl
-    inner join [OdionDataPlatform].[dbo].[mart_clienten] c
+from {{ ref('mart_wachtlijsten') }} wl
+    inner join {{ ref('mart_clienten') }} c
     on c.client_id = wl.client_id
-    left join hoofdlocaties hl
-    on hl.client_id = wl.client_id
-        and hl.rn = 1
-    left join #actieve_koppelingen ak
-    on ak.client_id = wl.client_id
-        and ak.cluster <> N'Wachtlijsten'
-        and (ak.einddatum is null or ak.einddatum >= cast(getdate() as date))
 
-group by
-    c.client_id,
-    c.clientnummer,
-    c.in_zorg,
-    c.leeftijd,
-    hl.hoofdlocatie_locatie_id,
-    hl.hoofdlocatie_locatienaam,
-    wl.wachtlijst_locatie_id,
-    wl.wachtlijst_locatienaam,
-    wl.wachtlijst_startdatum,
-    wl.wachtlijst_einddatum
+where wl.wachtlijst_niveau3 in (N'LG (Wachtlijst)', N'VG (Wachtlijst)')
 
 order by
     wl.wachtlijst_locatienaam,
