8377d4e6825f8dcb57ee1ff12903ab4cfb9ea7f1
Chantal van Son <c.vanson@odion.nl>
2026-09-16 15:24:04 +0200

    update mart_clienten_urls

--- models/marts/mart_clienten_urls.sql
+++ models/marts/mart_clienten_urls.sql
@@ -1,9 +1,10 @@
 select
     client_id,
-    {{ ons_administratie_url('client_id') }}              as url_ons_administratie,
-    {{ ons_dossier_url('care_plan', 'client_id') }}       as url_ons_zorgplan,
-    {{ ons_dossier_url('reports', 'client_id') }}         as url_ons_rapportages,
-    {{ ons_dossier_url('medical/overview', 'client_id') }} as url_ons_medicatie,
-    {{ ons_dossier_url('calendar', 'client_id') }}        as url_ons_agenda
+    {{ ons_administratie_url('client_id') }}                as url_ons_administratie,
+    {{ ons_dossier_url('care_plan', 'client_id') }}         as url_ons_zorgplan,
+    {{ ons_dossier_url('reports', 'client_id') }}           as url_ons_rapportages,
+    {{ ons_dossier_url('medical/overview', 'client_id') }}  as url_ons_medicatie,
+    {{ ons_dossier_url('calendar', 'client_id') }}          as url_ons_agenda,
+    {{ ons_dossier_url('survey_results', 'client_id') }}    as url_ons_vragenlijsten
 
 from {{ ref('stg_onsdb__clients') }}
